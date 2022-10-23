749efc258f3401701ddc15f85213bf3202413f67
Trongdat32 <302trongdat@gmail.com>
2022-10-23 14:04:18 +0700

    Them xu ly dieu kien nut cong va tru

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -17,6 +17,26 @@ namespace Calculator
             InitializeComponent();
         }
 
-        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtA.Text != String.Empty && txtB.Text != String.Empty)
+            {
+                double a = double.Parse(txtA.Text);
+                double b = double.Parse(txtB.Text);
+                double c = a + b;
+                txtKetQua.Text = c.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtA.Text != String.Empty && txtB.Text != String.Empty)
+            {
+                double a = double.Parse(txtA.Text);
+                double b = double.Parse(txtB.Text);
+                double c = a + b;
+                txtKetQua.Text = c.ToString();
+            }
+        }
     }
 }
